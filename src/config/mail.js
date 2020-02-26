91acf4f6cc28c083823454840007be4268f0d659
kahkurenai <sys1@grupopmz.com.br>
2020-02-26 16:27:58 -0400

    Ajustes

--- src/config/mail.js
+++ src/config/mail.js
@@ -4,9 +4,9 @@ export default {
   secure: false,
   auth: {
     user: '31083beec98df8',
-    pass: 'd075125ceedecb'
+    pass: 'd075125ceedecb',
   },
   default: {
-    from: 'Transportadora FastFeet <noreplay@fastfeet.com>'
-  }
-}
+    from: 'Transportadora FastFeet <noreplay@fastfeet.com>',
+  },
+};
